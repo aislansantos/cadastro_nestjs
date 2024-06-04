dd70f27b879860d7b7b72e9e88c6b697eef444d6
Aislan Santos <aislan.santos@gmail.com>
2024-06-04 14:03:15 -0300

    feat: add module prisma in seller.module

--- src/sellers/sellers.module.ts
+++ src/sellers/sellers.module.ts
@@ -1,8 +1,10 @@
 import { Module } from '@nestjs/common';
 import { SellersService } from './sellers.service';
 import { SellersController } from './sellers.controller';
+import { PrismaModule } from 'src/prisma/prisma.module';
 
 @Module({
+  imports: [PrismaModule],
   controllers: [SellersController],
   providers: [SellersService],
 })
