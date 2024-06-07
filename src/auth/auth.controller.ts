9ab3c03ffd033a6e60410202a2b6a295438315b6
Aislan Santos <aislan.santos@gmail.com>
2024-06-06 21:44:18 -0300

    criada as rotas e implmentedos alguns services referente a AUTH

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,21 +1,37 @@
 import { Body, Controller, Post } from '@nestjs/common';
 import { ApiTags } from '@nestjs/swagger';
+import { UsersService } from 'src/users/users.service';
 import { AuthService } from './auth.service';
+import { AuthForgetDto } from './dto/auth-forget.dto';
 import { AuthLoginDto } from './dto/auth-login.dto';
 import { AuthRegisterDto } from './dto/auth-register.dto';
+import { AuthResetDto } from './dto/auth-reset.dto';
 
-@ApiTags('Customers')
+@ApiTags('Auths')
 @Controller('auth')
 export class AuthController {
-  constructor(private readonly authService: AuthService) {}
+  constructor(
+    private readonly authService: AuthService,
+    private readonly userService: UsersService,
+  ) {}
 
   @Post('login')
-  public async login(@Body() body: AuthLoginDto) {}
+  public async login(@Body() { email, password }: AuthLoginDto) {
+    return this.authService.login(email, password);
+  }
 
   @Post('register')
-  public async register(@Body() body: AuthRegisterDto) {}
+  public async register(@Body() body: AuthRegisterDto) {
+    return this.userService.create(body);
+  }
 
-  // TODO: Fazer um metodo de solicitar recuperação de senha FORGET.
+  @Post('forget')
+  public async forget(@Body() { email }: AuthForgetDto) {
+    return this.authService.forget(email);
+  }
 
-  // TODO: Fazer um metodo de troca de senha RESET
+  @Post('reset')
+  public async reset(@Body() { password, token }: AuthResetDto) {
+    return this.authService.reset(password, token);
+  }
 }
--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -1,5 +1,7 @@
 import { Module } from '@nestjs/common';
 import { JwtModule } from '@nestjs/jwt';
+import { PrismaModule } from 'src/prisma/prisma.module';
+import { UsersModule } from 'src/users/users.module';
 import { AuthController } from './auth.controller';
 import { AuthService } from './auth.service';
 
@@ -8,6 +10,8 @@ import { AuthService } from './auth.service';
     JwtModule.register({
       secret: process.env.JWT_SECRET,
     }),
+    UsersModule,
+    PrismaModule,
   ],
   controllers: [AuthController],
   providers: [AuthService],
