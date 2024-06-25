d267e32464aa2d63362aa7dabea620ddec8375bf
Aislan Santos <aislan.santos@gmail.com>
2024-06-25 16:41:03 -0300

    ajustes de testes auth service

--- src/auth/auth.service.spec.ts
+++ src/auth/auth.service.spec.ts
@@ -1,4 +1,8 @@
 import { Test, TestingModule } from "@nestjs/testing";
+import { jwtServiceMock } from "../testing/jwt-service.mock";
+import { mailerServiceMock } from "../testing/mailer-service.mock";
+import { userRepositoryMock } from "../testing/user-repository.mock";
+import { userServiceMock } from "../testing/user-service.mock";
 import { AuthService } from "./auth.service";
 
 describe("AuthService", () => {
@@ -6,7 +10,13 @@ describe("AuthService", () => {
 
 	beforeEach(async () => {
 		const module: TestingModule = await Test.createTestingModule({
-			providers: [AuthService]
+			providers: [
+				AuthService,
+				userRepositoryMock,
+				jwtServiceMock,
+				userServiceMock,
+				mailerServiceMock
+			]
 		}).compile();
 
 		service = module.get<AuthService>(AuthService);
